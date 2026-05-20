fd633ae656c6cb5870f7877f6a500b768bd65e4e
Yousef Aljaberi+ <yousof4pro@gamil.com>
2026-05-20 08:49:20 +0300

    add solve Letter pattern and inverted pattern

--- Workspace/Workspace/Inverted_Number_Pattern.cpp
+++ Workspace/Workspace/Inverted_Number_Pattern.cpp
@@ -21,7 +21,7 @@ int readPositiveNumber(string message)
 void printInvertedPattern(int number)
 {
     for (int i = number; i > 0; i--)
-    {
+    {   
         for (int j = 1; j <= i; j++)
         {
             cout << i;
